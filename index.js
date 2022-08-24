241fa81fd59d2247a0a6dac324262b5a1a7ef305
Matias Sendra <mjs.sendra@gmail.com>
2022-08-23 22:40:39 -0300

    Ultimo commit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
+require('dotenv').config()
 
 
-const { leerInput, inquirerMenu, pausa, listarLugares } = require("./helpers/inquirer");
-const Busquedas = require("./models/busquedas");
-
+const { leerInput, inquirerMenu, pausa, listarLugares } = require('./helpers/inquirer');
+const Busquedas = require('./models/busquedas');
 
 const main = async () => {
     const busquedas = new Busquedas();
@@ -13,31 +12,48 @@ const main = async () => {
 
         opt = await inquirerMenu();
 
-        switch ( opt ) {
+        switch (opt) {
 
             case 1:
                 // Mostrar mensaje
-                const termino = await leerInput( 'Ciudad: ' );
-                
+                const termino = await leerInput('Ciudad: ');
+
                 // Buscar los lugares
-                const lugares = await busquedas.ciudad( termino );
-                
+                const lugares = await busquedas.ciudad(termino);
+
                 // Seleccionar el lugar
                 const id = await listarLugares(lugares);
-                const lugarSel = lugares.find( l => l.id == id );
-                console.log( lugarSel );
+                if (id === '0') continue; // si selecciono 0 vuelve al menu principal...
+
+                const lugarSel = lugares.find(l => l.id == id);
+                //console.log(lugarSel);
+
+                // Guardar en DB
+                busquedas.agregarHistorial(lugarSel.nombre);
 
                 // Clima
+                const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng);
+                //console.log(clima);
 
                 // Mostrar resultados
-
+                console.clear();
                 console.log('Información de la ciudad\n'.green);
-                console.log('Ciudad:', lugarSel.nombre );
-                console.log('Lat:', lugarSel.lat );
-                console.log('Lng:', lugarSel.lng );
-                console.log('Temperatura: ', );
-                console.log('Mínima: ', );
-                console.log('Máxima: ', );
+                console.log('Ciudad:', lugarSel.nombre.green);
+                console.log('Lat:', lugarSel.lat);
+                console.log('Lng:', lugarSel.lng);
+                console.log('Temperatura: ', clima.temp);
+                console.log('Mínima: ', clima.min);
+                console.log('Máxima: ', clima.max);
+                console.log('Cómo está el clima: ', clima.desc.green);
+                break;
+
+            case 2:
+                busquedas.historialCapitalizado.forEach((lugar, i) => {
+
+                    const idx = `${i + 1}.`.green;
+                    console.log(`${idx} ${lugar}`);
+
+                })
 
                 break;
 
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,13 +1,33 @@
+//Importaciones de NODE...
+const fs = require('fs')
+//Importaciones de terceros...
 const axios = require('axios');
 
 
 class Busquedas {
 
-    historial = ['Tegicugalpa', 'Madrid', 'San José'];
+    historial = [];
+    dbPath = './db/database.json';
 
     constructor() {
+
         // TODO: leer DB si exsite
+        this.leerDB();
+
     }
+
+    get historialCapitalizado(){
+        // Capitalizar cada palabra
+        return this.historial.map( lugar => {
+
+            let palabras = lugar.split(' ');
+            palabras = palabras.map( p => p[0].toUpperCase() + p.substring(1));
+
+            return palabras.join(' ')
+
+        });
+    }
+
 
     get paramsMapbox() {
 
@@ -17,6 +37,14 @@ class Busquedas {
             'language': 'es'
         }
 
+    }
+
+    get paramsWeather() {
+        return {
+            appid: process.env.OPENWEATHER_KEY,
+            units: 'metric',
+            lang: 'es'
+        }
     }
 
 
@@ -30,7 +58,7 @@ class Busquedas {
             });
 
             const resp = await instance.get();
-            return resp.data.features.map( lugar =>({
+            return resp.data.features.map(lugar => ({
                 id: lugar.id,
                 nombre: lugar.place_name,
                 lng: lugar.center[0],
@@ -41,11 +69,68 @@ class Busquedas {
 
             return [];
         }
+    }
 
-        return []; // retornar los lugares que coincidan con el lugar que paso la funcion...
+    async climaLugar(lat, lon) {
+
+        try {
+
+            const instance = axios.create({
+                baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+                params: { ...this.paramsWeather, lat, lon }
+            });
+
+            const resp = await instance.get();
+            const { weather, main } = resp.data;
+
+
+            return {
+                desc: weather[0].description,  // weather es un array de objetos, no un objeto directamente, por eso se pone la primer posicion.
+                min: main.temp_min,
+                max: main.temp_max,
+                temp: main.temp
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
 
     }
 
+    agregarHistorial( lugar = '' ){
+
+        if( this.historial.includes(lugar.toLocaleLowerCase()) ){
+            return;
+        }
+        this.historial = this.historial.splice(0,5);
+
+        // TODO: prevenir duplicados
+        this.historial.unshift( lugar.toLocaleLowerCase() );
+        // El método unshift() agrega uno o más elementos al inicio del array, y devuelve la nueva longitud del array, a diferencia del push que los añade al final del array...
+
+        // Grabar en DB
+        this.guardarDB();
+    }
+
+    guardarDB(){
+
+        const payload = {
+            historial: this.historial
+        }
+
+        fs.writeFileSync( this.dbPath, JSON.stringify( payload ));
+
+    }
+    leerDB(){
+
+        if( !fs.existsSync( this.dbPath ) ) return;
+
+        const info = fs.readFileSync( this.dbPath, { encoding: 'utf-8' });
+        const data = JSON.parse( info );
+        // El método JSON.parse() analiza una cadena de texto como JSON, transformando opcionalmente el valor producido por el análisis.
+
+        this.historial = data.historial;
+    }
 
 }
 
